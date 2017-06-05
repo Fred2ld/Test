e11729d53cb2348e4e930222919d9921d8f54710
Fred2ld <trotta.frederic@gmail.com>
2017-06-05 10:57:32 +0100

    Update TestGit.java

--- TestGit/src/testgit/TestGit.java
+++ TestGit/src/testgit/TestGit.java
@@ -15,7 +15,7 @@ public class TestGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("testgit.TestGit.main()");
+        System.out.println("testgit.TestGit.main().Edit_from_github");
     }
     
 }
